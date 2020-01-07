98bb43001343150b9054cf33e667a76ca8ad700d
Chang Xiong <cnxiong03@gmail.com>
2020-01-06 21:00:54 -0500

    video 6 complete

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const courseSchema = new mongoose.Schema({
      category: {
          type: String,
          required: true,
-         enum: ['web', 'mobile', 'network']
+         enum: ['web', 'mobile', 'network'],
+         lowercase: true,
+        //  uppercase: true,
+        trim: true
      },
     author: String,
     tags: {
@@ -38,6 +41,8 @@ const courseSchema = new mongoose.Schema({
         required: function() { return this.isPublished; },
         min: 10,
         max: 200,
+        get: v => Math.round(v),
+        set: v => Math.round(v)
     }
 });
 
@@ -46,11 +51,11 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
     const course = new Course({
         name: 'Angular Course',
-        category: '-',
+        category: 'Web',
         author: 'Chang',
-        tags: null,
+        tags: ['frontend'],
         isPublished: true,
-        price: 15
+        price: 15.8
     });
 
     try {
@@ -68,12 +73,12 @@ async function getCourses(){
     const pageSize = 10;
 
     const courses = await Course
-        .find({ author: 'Chang', isPublished: true })
-        .skip((pageNumber - 1) * pageSize)
-        .limit(pageSize)
+        .find({ _id: '5e13e41e0e2f9d68cec795be' })
+        // .skip((pageNumber - 1) * pageSize)
+        // .limit(pageSize)
         .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
-    console.log(courses);
+        .select({ name: 1, tags: 1, price: 1 });
+    console.log(courses[0].price);
 }
 
 async function updateCourse(id) {
@@ -91,4 +96,4 @@ async function removeCourse(id) {
     console.log(course);
 }
 
-createCourse();
+getCourses();
